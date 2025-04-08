2cb3bca58b3a7f0517f66775d31fe1c9c4072361
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-04-08 16:26:06 +0000

    c8

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,12 @@ data={
     "goals": [14,13,11,6,5],
     "assists": [4,5,3,5,4],
     "matches": [26,23,28,24,21],
-    "average distance [km]": [9.11,10.30,9.74,10.33,11.26],
-    "pass accuracy [%]": [74.5,78.6,76.9,83.8,90.7]
+    "average distance": [9.11,10.30,9.74,10.33,11.26],
+    "pass accuracy": [74.5,78.6,76.9,83.8,90.7]
 }
 
 df=pd.DataFrame(data)
+#st.dataframe(df)
 
 #Application
 st.title(":red[Serie A: top 5 players]")
@@ -32,5 +33,5 @@ st.write(">**Matches and assists line chart**")
 st.line_chart(df,x="players",y=["matches","assists"],color=["#44c544","#ff4035"])
 
 st.write(">**Pass accuracy and average distance covered area chart**")
-st.area_chart(df,x="players",y="pass accuracy [%]", color="#44c544")
-st.area_chart(df,x="players",y="average distance [km]", color="#44c544")
+st.area_chart(df,x="players",y="pass accuracy", color="#44c544")
+st.area_chart(df,x="players",y="average distance", color="#44c544")
